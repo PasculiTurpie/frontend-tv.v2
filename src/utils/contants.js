9dfaf802995b9406628e061160f0b55c73d3ce3a
PasculiTurpie <rodrigo.s.turpie@gmail.com>
2025-05-08 15:19:12 -0400

    feat:Objeto signal

---
+++ src/utils/contants.js
@@ -0,0 +1,22 @@
+const signal = [
+    {
+      id: 1,
+      name: "Canal 1",
+      type: "HD",
+      source: "IRD A"
+    },
+    {
+      id: 2,
+      name: "Canal 2",
+      type: "SD",
+      source: "IRD B"
+    }
+  ];
+  
+  // Exportación por defecto (default)
+  export default signal;
+  
+  // O exportación con nombre
+  // export { signal };
+  
+  
