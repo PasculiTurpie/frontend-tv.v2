8e3eaa1a9ba6e3344e513d8032a6e26706e4f184
PasculiTurpie <rodrigo.s.turpie@gmail.com>
2025-05-09 14:55:36 -0400

    feat:Confeccionando Card 3

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './Card.css'
+import "./Card.css";
 import signal from "../../utils/contants";
 
 const Card = () => {
@@ -8,20 +8,30 @@ const Card = () => {
     return (
         <>
             {signal.map((signalItem, index) => {
-                  return (<div className="card__container" key={index} value={signalItem.id}>
-                        <h4>{signalItem.nameChannel}</h4>
-                        <div className='card__number'>
-                        <h5 className="card__number-item">{`Norte: ${signalItem.numberChannelCn}`}</h5>
+                return (
+                    <div
+                        className="card__container"
+                        key={index}
+                        value={signalItem.id}
+                    >
+                    <div className="card__group-item">
+                        <h4 className="card_title">{signalItem.nameChannel}</h4>
+                        <div className="card__number">
+                            <h5 className="card__number-item">{`Norte: ${signalItem.numberChannelCn}`}</h5>
 
-                        <h5 className="card__number-item">{`Sur: ${signalItem.numberChannelSur}`}</h5>
+                            <h5 className="card__number-item">{`Sur: ${signalItem.numberChannelSur}`}</h5>
                         </div>
+
+
+                    </div>
                         <img
                             className="card__logo"
                             src={signalItem.logoChannel}
                         />
-                        <p className="card__severidad">{`Severidad: ${signalItem.severidadChannel}`}</p>
+                        <span className="card__severidad">{`Severidad: ${signalItem.severidadChannel}`}</span>
                         {/* <p>{signalItem.satelite.nameSatelite}</p> */}
-                    </div>)
+                    </div>
+                );
             })}
         </>
     );
