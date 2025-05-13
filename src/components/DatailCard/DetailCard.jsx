0ecce01a8e8f503aa51560c752f8e1ccff000dd7
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2025-05-13 16:14:58 -0400

    feat:implementando Card Detail

--- src/components/DatailCard/DetailCard.jsx
+++ src/components/DatailCard/DetailCard.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import './DetailCard.css'
 
 const DetailCard = () => {
+  const { id } = useParams()
+  const [DetailCard, setDetailCard] = useState({})
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/api/v2/signal/${id}`)
+      .then((response) => {
+        console.log(response.data)
+        setDetailCard(response.data)
+      })
+  }, [])
+
   return (
     <>
-      
+      <div className='container__card-detail'>
+        <div className='card-detail-container'>
+          <div className='card-detail-header'>
+          <img className='card__detail-logo' src={DetailCard.logoChannel}></img>
+            <h3 className='card-detail-title'>{DetailCard.nameChannel}</h3>
+
+          </div>
+          <div className='card__detail-numbers'>
+            <span>Número Cn: {DetailCard.numberChannelCn}</span>
+          <span>Número Sur: {DetailCard.numberChannelSur }</span>
+          </div>
+          <p>{DetailCard.tipoTecnologia}</p>
+          <p>Severidad: {DetailCard.severidadChannel}</p>
+          <div className='card__detail-button'> 
+
+          <button className='button btn-success'>Contacto</button>
+          <button className='button btn-primary'>Ver mapa</button>
+          </div>
+
+        </div>
+
+      </div>
 
     </>
   )
--- src/components/Diagrama/Diagrama.jsx
+++ src/components/Diagrama/Diagrama.jsx
@@ -3,18 +3,7 @@ import { useParams } from 'react-router-dom'
 
 const Diagrama = ({ id }) => {
 
-  const {params} = useParams()
-console.log(params)
-
-  useEffect(() => {
-    axios.get(`http://localhost:3000/api/v2/signal/${id}`)
-      console.log(id)
-      .then((response) => {
-        console.log(response.data)
-        setSignalTv(response.data)
-      })
-  }, [])
-
+ 
   return (
     <>
 
