04c5296965983787ee8480827f3bb5302e5b29a9
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2025-05-28 16:19:47 -0400

    feat:Corrección en la validación de passwords en user

--- src/components/User/ModalUser.jsx
+++ src/components/User/ModalUser.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ModalComponent from "../ModalComponent/ModalComponent";
 import api from "../../utils/api";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import Swal from "sweetalert2";
 
@@ -16,26 +16,27 @@ const EditSchemaUser = Yup.object().shape({
                 value?.startsWith("http://") || value?.startsWith("https://")
         )
         .url("Debe ser una URL válida"),
-  password: Yup.string()
-    .nullable()
-    .test(
-      "optional-password",
-      "Debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un símbolo",
-      function (value) {
-        if (!value) return true; // Permitir vacío
-        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(value);
-      }
+    password: Yup.string()
+        .nullable()
+        .test(
+            "optional-password",
+            "No cumple con las reglas",
+            function (value) {
+                if (!value) return true;
+                return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(
+                    value
+                );
+            }
         ),
     confirmPassword: Yup.string().oneOf(
         [Yup.ref("password")],
-        "Las contraseñas deben coincidir"
+        "Las contraseñas no coincidir"
     ),
     rol: Yup.string(),
 });
 
 const ModalUser = ({ itemId, modalOpen, setModalOpen, title, refreshList }) => {
     const [dataUser, setDataUser] = useState(null);
-    const [showPasswordFields, setShowPasswordFields] = useState(false);
 
     useEffect(() => {
         if (itemId) {
@@ -44,157 +45,172 @@ const ModalUser = ({ itemId, modalOpen, setModalOpen, title, refreshList }) => {
             });
         }
     }, [itemId]);
-  
-  
 
     if (!dataUser) return null;
 
     return (
-        <>
-            <Formik
-                enableReinitialize={true}
-                initialValues={{
-                    username: dataUser.username || "",
-                    email: dataUser.email || "",
-                    password: "",
-                    confirmPassword: "",
-                    profilePicture: dataUser.profilePicture || "",
-                    rol: dataUser.rol || "",
-                }}
-                validationSchema={EditSchemaUser}
-          onSubmit={async (values, { resetForm }) => {
-            console.log("ID que se está enviando:", itemId);
-                    try {
-                        await api.updateUserId(itemId, values);
-                        Swal.fire({
-                            icon: "success",
-                            title: "Usuario actualizado",
-                            text: "El usuario se ha actualizado exitosamente!",
-                            footer: `<h4>${values.email}</h4>`,
-                        });
-                        refreshList()
-                        setModalOpen(false);
-                        resetForm();
-                    } catch (error) {
-                        Swal.fire({
-                            icon: "error",
-                            title: "Ups!!",
-                            text: `${
-                                error.response?.data?.message ||
-                                "Error desconocido"
-                            }`,
-                        });
-                        console.error(error);
-                    }
-                }}
-            >
-                {() => (
-                    <ModalComponent
-                        modalOpen={modalOpen}
-                        title={title}
-                        setModalOpen={setModalOpen}
-                    >
-                        <Form className="grid-cols-2">
-                            <div>
-                                <div className="form__group">
-                                    <label className="form__group-label">
-                                        Nombre usauario
-                                        <br />
-                                        <Field
-                                            type="text"
-                                            className="form__group-input"
-                                            placeholder="Nombre"
-                                            name="username"
-                                        />
-                                    </label>
-                                    <div className="form__group-error"></div>
-                                </div>
-                                <div className="form__group">
-                                    <label className="form__group-label">
-                                        Email
-                                        <br />
-                                        <Field
-                                            type="text"
-                                            className="form__group-input"
-                                            placeholder="Email"
-                                            name="email"
-                                        />
-                                    </label>
-                                    <div className="form__group-error"></div>
-                                </div>
-                                <div className="form__group">
-                                    <label className="form__group-label">
-                                        Avatar
-                                        <br />
-                                        <Field
-                                            type="text"
-                                            className="form__group-input"
-                                            placeholder="Avatar"
-                        name="profilePicture"
-                                        />
-                                    </label>
-                                    <div className="form__group-error"></div>
-                                </div>
+        <Formik
+            enableReinitialize={true}
+            initialValues={{
+                username: dataUser.username || "",
+                email: dataUser.email || "",
+                password: "",
+                confirmPassword: "",
+                profilePicture: dataUser.profilePicture || "",
+                rol: dataUser.rol || "",
+            }}
+            validationSchema={EditSchemaUser}
+            onSubmit={async (values, { resetForm }) => {
+                try {
+                    await api.updateUserId(itemId, values);
+                    Swal.fire({
+                        icon: "success",
+                        title: "Usuario actualizado",
+                        text: "El usuario se ha actualizado exitosamente!",
+                        footer: `<h4>${values.email}</h4>`,
+                    });
+                    refreshList();
+                    setModalOpen(false);
+                    resetForm();
+                } catch (error) {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Ups!!",
+                        text: `${
+                            error.response?.data?.message || "Error desconocido"
+                        }`,
+                    });
+                    console.error(error);
+                }
+            }}
+        >
+            {() => (
+                <ModalComponent
+                    modalOpen={modalOpen}
+                    title={title}
+                    setModalOpen={setModalOpen}
+                >
+                    <Form className="grid-cols-2">
+                        <div>
+                            <div className="form__group">
+                                <label className="form__group-label">
+                                    Nombre usuario
+                                    <br />
+                                    <Field
+                                        type="text"
+                                        className="form__group-input"
+                                        placeholder="Nombre"
+                                        name="username"
+                                    />
+                                    <ErrorMessage
+                                        name="username"
+                                        component="div"
+                                        className="form__group-error"
+                                    />
+                                </label>
                             </div>
+                            <div className="form__group">
+                                <label className="form__group-label">
+                                    Email
+                                    <br />
+                                    <Field
+                                        type="text"
+                                        className="form__group-input"
+                                        placeholder="Email"
+                                        name="email"
+                                    />
+                                    <ErrorMessage
+                                        name="email"
+                                        component="div"
+                                        className="form__group-error"
+                                    />
+                                </label>
+                            </div>
+                            <div className="form__group">
+                                <label className="form__group-label">
+                                    Avatar
+                                    <br />
+                                    <Field
+                                        type="text"
+                                        className="form__group-input"
+                                        placeholder="Avatar"
+                                        name="profilePicture"
+                                    />
+                                    <ErrorMessage
+                                        name="profilePicture"
+                                        component="div"
+                                        className="form__group-error"
+                                    />
+                                </label>
+                            </div>
+                        </div>
 
-                            <div>
-                                <div className="form__group">
-                                    <label className="form__group-label">
-                                        Rol
-                                        <br />
-                                        <Field
-                                            as="select"
-                                            type="select"
-                                            className="form__group-input"
-                                            name="rol"
-                                        >
-                                            <option value="0">
-                                                Seleccionar
-                                            </option>
-                                            <option value="admin">admin</option>
-                                            <option value="user">user</option>
-                                        </Field>
-                                    </label>
-                                    <div className="form__group-error"></div>
-                                </div>
-                                <div className="form__group">
-                                    <label className="form__group-label">
-                                        Contraseña
-                                        <br />
-                                        <Field
-                                            type="text"
-                                            className="form__group-input"
-                                            placeholder="Contraseña"
-                                            name="password"
-                                        />
-                                    </label>
-                                    <div className="form__group-error"></div>
-                                </div>
-                                <div className="form__group">
-                                    <label className="form__group-label">
-                                        Confirmar contraseña
-                                        <br />
-                      <Field
-                                            type="text"
-                                            className="form__group-input"
-                                            placeholder="Contraseña"
-                                            name="confirmPaswword"
-                                        />
-                                    </label>
-                                    <div className="form__group-error"></div>
-                                </div>
+                        <div>
+                            <div className="form__group">
+                                <label className="form__group-label">
+                                    Rol
+                                    <br />
+                                    <Field
+                                        as="select"
+                                        className="form__group-input"
+                                        name="rol"
+                                    >
+                                        <option value="0">Seleccionar</option>
+                                        <option value="admin">admin</option>
+                                        <option value="user">user</option>
+                                    </Field>
+                                    <ErrorMessage
+                                        name="rol"
+                                        component="div"
+                                        className="form__group-error"
+                                    />
+                                </label>
                             </div>
-                            <button
-                                type="submit"
-                                className="button btn-primary btn-adjust"
-                            >
-                                Actualizar
-                            </button>
-                        </Form>
-                    </ModalComponent>
-                )}
-            </Formik>
-        </>
+                            <div className="form__group">
+                                <label className="form__group-label">
+                                    Contraseña
+                                    <br />
+                                    <Field
+                                        type="password"
+                                        className="form__group-input"
+                                        placeholder="Contraseña"
+                                        name="password"
+                                    />
+                                    <ErrorMessage
+                                        name="password"
+                                        component="div"
+                                        className="form__group-error"
+                                    />
+                                </label>
+                            </div>
+                            <div className="form__group">
+                                <label className="form__group-label">
+                                    Confirmar contraseña
+                                    <br />
+                                    <Field
+                                        type="password"
+                                        className="form__group-input"
+                                        placeholder="Confirmar contraseña"
+                                        name="confirmPassword"
+                                    />
+                                    <ErrorMessage
+                                        name="confirmPassword"
+                                        component="div"
+                                        className="form__group-error"
+                                    />
+                                </label>
+                            </div>
+                        </div>
+                        <button
+                            type="submit"
+                            className="button btn-primary btn-adjust"
+                        >
+                            Actualizar
+                        </button>
+                    </Form>
+                </ModalComponent>
+            )}
+        </Formik>
     );
 };
 
