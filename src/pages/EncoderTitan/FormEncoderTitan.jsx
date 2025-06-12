0600326fc496ab395b10064710c9574eab85a9b7
PasculiTurpie <rodrigo.s.turpie@gmail.com>
2025-06-12 09:17:04 -0400

    feat:Implementación de CRUD Titan

--- src/pages/EncoderTitan/FormEncoderTitan.jsx
+++ src/pages/EncoderTitan/FormEncoderTitan.jsx
@@ -4,6 +4,28 @@ import * as Yup from "yup";
 import Swal from "sweetalert2";
 import api from "../../utils/api";
 
+const ipMulticastRegex =
+    /^(2(?:[0-4]\d|5[0-5])\.(?:[0-9]{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(?:[0-9]{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(?:[0-9]{1,2}|1\d\d|2[0-4]\d|25[0-5]))$/;
+
+const ipGestionRegex = /172.19.14\.\d/;
+
+const RegisterSchemaTitan = Yup.object().shape({
+    nombreTitan: Yup.string(),
+    urlTitan: Yup.string().matches(
+        /(?:https?\:\/\/\w+\.\w+\.\w+.+)/,
+        "Ingresa una url válida"
+    ),
+    mcastIn: Yup.string().matches(
+        ipMulticastRegex,
+        "Debe ser una multicast válida"
+    ),
+    mcastOut: Yup.string().matches(
+        ipMulticastRegex,
+        "Debe ser una multicast válida"
+    ),
+    ipGestion: Yup.string().matches(ipGestionRegex, "Debe ser una ip válida"),
+});
+
 const FormEncoderTitan = () => {
   return (
     <>
@@ -21,7 +43,142 @@ const FormEncoderTitan = () => {
             </li>
           </ol>
         </nav>
-        <h1>Formulario Titan</h1>
+        <Formik
+        initialValues={
+           {nombreTitan: "",
+                    urlTitan: "",
+                    mcastIn: "",
+                    mcastOut: "",
+                    ipGestion: "",
+                    }
+        }
+        validationSchema={RegisterSchemaTitan}
+         enableReinitialize={true}
+                    onSubmit={async (values, { resetForm }) => {
+                        try {
+                            const response = await api.createTitan(values);
+                            console.log(response);
+                            Swal.fire({
+                                title: "Titan guardado exitosamente",
+                                icon: "success",
+                                html: `
+                                <p><strong>Nombre Titan:</strong> ${values.nombreTitan}</p>
+                                <p><strong>Modelo:</strong> ${values.ipGestion}</p>
+                              `,
+                            });
+                          resetForm();
+                        } catch (error) {
+                            console.log(error);
+                            Swal.fire({
+                                title: "Error",
+                                icon: "error",
+                                text: `Duplicidad de datos`,
+                                footer: `${error.response.data.message}`,
+                            });
+                        }
+                    }}
+        
+        >
+        {({errors, touched })=>(
+
+          <Form className="form__add">
+             <div className="form__group">
+                                            <label
+                                                htmlFor="nombreTitan"
+                                                className="form__group-label"
+                                            >
+                                                Nombre Titan
+                                                <br />
+                                                <Field
+                                                    type="text"
+                                                    className="form__group-input"
+                                                    placeholder="Nombre titan"
+                                                    name="nombreTitan"
+                                                />
+                                            </label>
+            
+                                            {errors.nombreTitan && touched.nombreTitan ? (
+                                                <div className="form__group-error">
+                                                    {errors.nombreTitan}
+                                                </div>
+                                            ) : null}
+                                        </div>
+            
+                                        <div className="form__group">
+                                            <label
+                                                htmlFor="urlTitan"
+                                                className="form__group-label"
+                                            >
+                                                Multicast In Titan
+                                                <br />
+                                                <Field
+                                                    type="text"
+                                                    className="form__group-input"
+                                                    placeholder=" Multicast In"
+                                                    name="mcastIn"
+                                                />
+                                            </label>
+            
+                                            {errors.mcastIn && touched.mcastIn ? (
+                                                <div className="form__group-error">
+                                                    {errors.mcastIn}
+                                                </div>
+                                            ) : null}
+                                        </div>
+            
+                                        <div className="form__group">
+                                            <label
+                                                htmlFor="mcastOut"
+                                                className="form__group-label"
+                                            >
+                                                Multicast Out Titan
+                                                <br />
+                                                <Field
+                                                    type="text"
+                                                    className="form__group-input"
+                                                    placeholder="Multicast Out"
+                                                    name="mcastOut"
+                                                />
+                                            </label>
+            
+                                            {errors.mcastOut && touched.mcastOut ? (
+                                                <div className="form__group-error">
+                                                    {errors.mcastOut}
+                                                </div>
+                                            ) : null}
+                                        </div>
+            
+                                        <div className="form__group">
+                                            <label
+                                                htmlFor="ipGestion"
+                                                className="form__group-label"
+                                            >
+                                                Ip Gestión Titan
+                                                <br />
+                                                <Field
+                                                    type="text"
+                                                    className="form__group-input"
+                                                    placeholder="Ip gestión"
+                                                    name="ipGestion"
+                                                />
+                                            </label>
+            
+                                            {errors.ipGestion && touched.ipGestion ? (
+                                                <div className="form__group-error">
+                                                    {errors.ipGestion}
+                                                </div>
+                                            ) : null}
+                                        </div>
+                                        <button
+                                            type="submit"
+                                            className={`button btn-primary`}
+                                        >
+                                            Enviar
+                                        </button>
+          </Form>
+
+        )}
+        </Formik>
       </div>
     </>
   )
