0742cf83565d1d4086203375e31f7252f0b5ecbf
PasculiTurpie <rodrigo.s.turpie@gmail.com>
2025-07-01 12:31:58 -0400

    feat:Implementación de modal contacto

--- src/components/DatailCard/DetailCard.jsx
+++ src/components/DatailCard/DetailCard.jsx
@@ -2,34 +2,42 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import "./DetailCard.css";
-import api from '../../utils/api'
+import api from "../../utils/api";
+import ModalContact from "./ModalContact";
 
 const DetailCard = () => {
     const { id } = useParams();
     const [DetailCard, setDetailCard] = useState({});
-    const navigate = useNavigate()
-     const location = useLocation()
+    const navigate = useNavigate();
+    const location = useLocation();
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
 
     useEffect(() => {
-        api.getIdSignal(id)
-            .then((response) => {
-
-                setDetailCard(response.data);
-            });
+        api.getIdSignal(id).then((response) => {
+            setDetailCard(response.data);
+        });
     }, []);
 
-    const handleClickDiagram = ()=>{
-       navigate(`/diagrama/${id}`)
-    }
+    const handleClickDiagram = () => {
+        navigate(`/diagrama/${id}`);
+    };
     const handleBackSubmit = () => {
         navigate(`/`);
-    }
+    };
     return (
         <>
             <div className="container__card-detail">
                 <div className="card-detail-container">
-                    <button className="button-back button btn-warning" onClick={handleBackSubmit}>Volver</button>
+                    <button
+                        className="button-back button btn-warning"
+                        onClick={handleBackSubmit}
+                    >
+                        Volver
+                    </button>
                     <div className="card-detail-header">
                         <img
                             className="card__detail-logo"
@@ -46,11 +54,21 @@ const DetailCard = () => {
                     <p>{DetailCard.tipoTecnologia}</p>
                     <p>Severidad: {DetailCard.severidadChannel}</p>
                     <div className="card__detail-button">
-                        <button className="button btn-success">Contacto</button>
-                        <button onClick={handleClickDiagram} className="button btn-primary">Ver mapa</button>
+                        <button className="button btn-success" onClick={openModal}>Contacto</button>
+                        <button
+                            onClick={handleClickDiagram}
+                            className="button btn-primary"
+                        >
+                            Ver mapa
+                        </button>
                     </div>
                 </div>
             </div>
+            <ModalContact isOpen={isModalOpen} onClose={closeModal}>
+            <h2>Hola desde el modal</h2>
+        <p>Este es el contenido del modal.</p>
+
+            </ModalContact>
         </>
     );
 };
