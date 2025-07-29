0ca0a5b5048bad4c64b31eee0c52539e06dccc15
PasculiTurpie <rodrigo.s.turpie@gmail.com>
2025-07-29 17:12:48 -0400

    feat:Se realiza función para oedenar por numberChannelSur en Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -14,21 +14,28 @@ const Card = () => {
     const [hasError, setHasError] = useState(false);
 
     const getAllSignal = async () => {
-        try {
-            const response = await api.getSignal();
-            if (response.data.length > 0) {
-                setSignalTv(response.data);
-                setHasError(false);
-            } else {
-                setHasError(true); // No se encontraron datos
-            }
-        } catch (error) {
-            console.error("Error al obtener las señales:", error);
-            setHasError(true);
-        } finally {
-            setIsLoading(false);
+    try {
+        const response = await api.getSignal();
+        if (response.data.length > 0) {
+            // Convertir a número y ordenar por numberChannelSur
+            const sortedData = response.data.sort((a, b) => {
+                const numA = Number(a.numberChannelSur);
+                const numB = Number(b.numberChannelSur);
+                return numA - numB;
+            });
+
+            setSignalTv(sortedData);
+            setHasError(false);
+        } else {
+            setHasError(true); // No se encontraron datos
         }
-    };
+    } catch (error) {
+        console.error("Error al obtener las señales:", error);
+        setHasError(true);
+    } finally {
+        setIsLoading(false);
+    }
+};
 
     useEffect(() => {
         refreshList()
